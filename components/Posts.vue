a229e1e12bd3de241c09f634c517ffd6054c3b0f
kanes02829 <kanes02829@gmail.com>
2019-12-02 00:34:17 -0300

    fake post layout, default.vue, index and about pages

---
+++ components/Posts.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="main-content">
+    <div class="container">
+      <h1 class="title has-text-centered">Recent Posts.</h1>
+      <div class="columns is-multiline">
+        <div class="column is-half">
+          <div class="card">
+            <header class="card-header">
+              <p class="card-header-title">Título</p>
+            </header>
+            <div class="card-content">
+              <div class="content">
+                Sumário
+                <br />
+                <small>
+                  by
+                  <strong>Autor</strong>
+                  \\ Publicado
+                </small>
+              </div>
+            </div>
+            <footer class="card-footer">
+              <a href="/post/id" class="card-footer-item">Read More</a>
+            </footer>
+          </div>
+          <div class="card">
+            <header class="card-header">
+              <p class="card-header-title">Título</p>
+            </header>
+            <div class="card-content">
+              <div class="content">
+                Sumário
+                <br />
+                <small>
+                  by
+                  <strong>Autor</strong>
+                  \\ Publicado
+                </small>
+              </div>
+            </div>
+            <footer class="card-footer">
+              <a href="/post/id" class="card-footer-item">Read More</a>
+            </footer>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: "posts"
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,26 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-ssr-blog-from-scratch
-      </h1>
-      <h2 class="subtitle">
-        A blog template SSR with Nuxt and Vue
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div>
+    <section class="hero is-medium is-primary is-bold">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">Welcome to the JavaScript SSR Blog.</h1>
+          <h2 class="subtitle">Hope you find something you like.</h2>
+        </div>
       </div>
-    </div>
+    </section>
+    <posts />
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Posts from "~/components/Posts.vue";
 
 export default {
   components: {
-    Logo
+    Posts
+  },
+  head: {
+    title: "Home"
   }
-}
+};
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>

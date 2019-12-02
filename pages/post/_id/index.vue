a19025a66b4ceea00a75b0d85727472f12163154
kanes02829 <kanes02829@gmail.com>
2019-12-02 00:51:17 -0300

    add post/_id page template

---
+++ pages/post/_id/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main-content">
+    <div class="container">
+      <h2 class="title is-2">Titulo</h2>
+      <div>Conteúdo</div>
+      <br />
+      <h4 class="title is-5 is-marginless">
+        by
+        <strong>author</strong> at
+        <strong>published</strong>
+      </h4>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>

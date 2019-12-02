4d1c6d1bdeecbfcbbd7cd3059e88918bf50b5a46
kanes02829 <kanes02829@gmail.com>
2019-12-02 00:41:16 -0300

    add posts.json and data()

--- components/Posts.vue
+++ components/Posts.vue
@@ -3,38 +3,19 @@
     <div class="container">
       <h1 class="title has-text-centered">Recent Posts.</h1>
       <div class="columns is-multiline">
-        <div class="column is-half">
+        <div class="column is-half" v-for="post in posts" :key="post.id">
           <div class="card">
             <header class="card-header">
-              <p class="card-header-title">Título</p>
+              <p class="card-header-title">{{post.title}}</p>
             </header>
             <div class="card-content">
               <div class="content">
-                Sumário
+                {{post.summary}}
                 <br />
                 <small>
                   by
-                  <strong>Autor</strong>
-                  \\ Publicado
-                </small>
-              </div>
-            </div>
-            <footer class="card-footer">
-              <a href="/post/id" class="card-footer-item">Read More</a>
-            </footer>
-          </div>
-          <div class="card">
-            <header class="card-header">
-              <p class="card-header-title">Título</p>
-            </header>
-            <div class="card-content">
-              <div class="content">
-                Sumário
-                <br />
-                <small>
-                  by
-                  <strong>Autor</strong>
-                  \\ Publicado
+                  <strong>{{post.author}}</strong>
+                  \\ {{post.published}}
                 </small>
               </div>
             </div>
@@ -48,7 +29,11 @@
   </section>
 </template>
 <script>
+import posts from "~/posts.json";
 export default {
-  name: "posts"
+  name: "posts",
+  data() {
+    return { posts };
+  }
 };
 </script>
